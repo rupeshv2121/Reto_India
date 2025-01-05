413e906d808d123d144d27ae540186b388139cb5
Rupesh Varshney <146447594+rupeshv2121@users.noreply.github.com>
2025-01-06 01:36:44 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+// import Navbar from "./components/Navbar";
+import CheckoutPage from "./components/CheckOutPage/CheckoutPage";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar /> */}
+
+      <CheckoutPage />
+    </>
+  );
+}
+
+export default App;
