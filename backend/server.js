20c7538a2598a89bcca9ae2369d37172d526ad21
Rupesh Varshney <146447594+rupeshv2121@users.noreply.github.com>
2025-01-13 11:55:16 +0530

    Fixed the issue in product page

--- backend/server.js
+++ backend/server.js
@@ -48,31 +48,6 @@ const ProductModel = newDatabaseConnection.model('Product', Product.schema);
 const ReviewModel = newDatabaseConnection.model('Review', Review.schema);
 const ContactInfoModel = newDatabaseConnection.model('ContactInfo', ContactInfo.schema);
 
-const addProducts = async () => {
-    try {
-        const products = await Product.create([
-            {
-                OrderId: "p28cs2",
-                name: "SOFA",
-                Rating: 4,
-                NoOfRating: 421,
-                Price: 4000,
-                Discount: "40%",
-                OfferPrice: 2400,
-                ProdDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl ac laoreet bibendum, metus felis euismod dolor, ac varius nunc purus sit amet nunc. euismod dolor, ac varius nunc purus sit amet nunc.",
-                HeadImg: "/uploads/sofa_img1.jpg",
-                Img1: "/uploads/sofa_img2.jpeg",
-                Img2: "/uploads/sofa_img3.jpg",
-                Img3: "/uploads/sofa_img2.jpeg",
-                Img4: "/uploads/sofa_img4.jpg",
-            }
-        ]);
-        console.log(products)
-    } catch (error) {
-        console.error("Error adding products:", error);
-    }
-};
-addProducts();
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
