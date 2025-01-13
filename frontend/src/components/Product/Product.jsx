1d37967e996b8e6b1b85b425613a81917f5713f2
Rupesh Varshney <146447594+rupeshv2121@users.noreply.github.com>
2025-01-13 11:53:32 +0530

    Fixes Issues in Product Page

--- frontend/src/components/Product/Product.jsx
+++ frontend/src/components/Product/Product.jsx
@@ -1,11 +1,8 @@
+import StarRateIcon from "@mui/icons-material/StarRate";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./Product.css";
 import Review from "./Reviews/Review";
-import SofaImg1 from "./SofaImgBox/SofaImg/sofa_img1.jpg";
-import SofaImg2 from "./SofaImgBox/SofaImg/sofa_img2.jpeg";
-import SofaImg3 from "./SofaImgBox/SofaImg/sofa_img3.jpg";
-import SofaImg4 from "./SofaImgBox/SofaImg/sofa_img4.jpg";
 
 const Product = () => {
   const [data, setData] = useState([{}]);
@@ -29,8 +26,7 @@ const Product = () => {
         <div className="ProdSection">
           <div className="ProdHead">
             <img
-              // src={`http://localhost:3000${data[0].HeadImg}`}
-              src={SofaImg1}
+              src={`http://localhost:5000${data[0].HeadImg}`}
               alt="sofa"
               className="sofa_img_head"
             />
@@ -38,26 +34,22 @@ const Product = () => {
 
           <div className="sofa_other">
             <img
-              // src={`http://localhost:3000${data[0].Img1}`}
-              src={SofaImg2}
+              src={`http://localhost:5000${data[0].Img1}`}
               alt="bed"
               className="sofa_img"
             />
             <img
-              // src={`http://localhost:3000${data[0].Img2}`}
-              src={SofaImg3}
+              src={`http://localhost:5000${data[0].Img2}`}
               alt="chair"
               className="sofa_img"
             />
             <img
-              // src={`http://localhost:3000${data[0].Img3}`}
-              src={SofaImg4}
+              src={`http://localhost:5000${data[0].Img3}`}
               alt="chair"
               className="sofa_img"
             />
             <img
-              // src={`http://localhost:3000${data[0].Img4}`}
-              src={SofaImg1}
+              src={`http://localhost:5000${data[0].Img4}`}
               alt="chair"
               className="sofa_img"
             />
@@ -68,7 +60,7 @@ const Product = () => {
           <div className="content">
             <div id="header">
               <div>
-                {/* <div>
+                <div>
                   <h1 className="content_heading">{data[0].name}</h1>
                   <div className="rating">
                     {Array.from({ length: data[0].Rating }, (_, i) => (
@@ -81,21 +73,21 @@ const Product = () => {
                       {data[0].Rating} ({data[0].NoOfRating})
                     </p>
                   </div>
-                </div> */}
-                {/* 
+                </div>
+
                 <div>
                   <h2>
                     INR {data[0].Price} ({data[0].Discount} off)
                   </h2>
                   <h2>Offer Price: INR {data[0].OfferPrice}</h2>
-                </div> */}
+                </div>
 
-                {/* <div>
+                <div>
                   <h1 id="description">About this Gem</h1>
                   <div className="prod_description">
                     <p>{data[0].ProdDescription}</p>
                   </div>
-                </div> */}
+                </div>
               </div>
 
               <div className="BtnBar">
