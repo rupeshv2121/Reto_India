4a50e2e213e9edb582369b399c6bb1ebe5074a19
Rupesh Varshney <146447594+rupeshv2121@users.noreply.github.com>
2025-01-29 18:49:50 +0530

    Fixed Bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./App.css";
+import CartPage from "./components/CartPage/CartPage";
 import CheckoutPage from "./components/CheckOutPage/CheckoutPage";
 import Contact from "./components/Contact/Contact";
 import LandingPage from "./components/LandingPage/LandingPage";
-import CartPage from "./components/CartPage/CartPage";
 import Product from "./components/ProductPage/ProductPage.jsx";
 // import LandingPageSm from "./components/LandingPageSm/LandingPageSm";
 import MainLayout from "./components/Layout/MainLayout";
 import Login from "./components/Login_Signup_Page/Login";
 import Signup from "./components/Login_Signup_Page/Signup";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import ProductView from "./components/ProductView/ProductView";
-=======
-=======
->>>>>>> 8de5bf848e10ff1ab746314d566ab08ebeeb9477
+// import ProductView from "./components/ProductView/ProductView";
+
 // import ProductView from "./components/Product/Product";
 import ProductView from "./components/ProductView/ProductView.jsx";
->>>>>>> 8de5bf8 (some other changes)
-import TrackingPage from "./components/Track_order/TrackingPage";
+
 import OrderPage from "./components/OrderPage/OrderPage.jsx";
+import TrackingPage from "./components/Track_order/TrackingPage";
 
 const router = createBrowserRouter([
   {
