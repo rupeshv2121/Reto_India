1561a9e86c9a600aefa998b49bca7695a8944619
Rupesh Varshney <146447594+rupeshv2121@users.noreply.github.com>
2025-01-31 06:24:26 +0530

    Fixed issues

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,91 +1,10 @@
-import React from "react";
+// import React from "react";
 import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
-    <>
-      {/* <!-- Header Section --> */}
+    <div className="landing">
       <header>
-        {/* <div className="logo">
-          <img src="/img/LogoWoBcg.png" alt="Logo" />
-        </div> */}
-        {/* <!-- Desktop landing-navigation --> */}
-        {/* <landing-nav>
-          // <a href="#home">
-            <i
-              className="fa-solid fa-house ho-me"
-              style={{ color: "#f58634" }}
-            ></i>
-            Home
-          </a>
-          <a href="#products">
-            <i
-              className="fa-solid fa-cart-shopping pro-dct"
-              style={{ color: "#f58634" }}
-            ></i>
-            Products
-          </a>
-          <a href="#about">
-            <i
-              className="fa-solid fa-book ab-ut"
-              style={{ color: "#f58634" }}
-            ></i>
-            About
-          </a>
-          <a href="#login">
-            <span className="lo-gin">
-              <i className="fa-solid fa-user lo-gin"></i>Pooja Naitam
-            </span>
-          </a>
-          <a href="#contact">
-            <i
-              className="fa-solid fa-phone con-tct"
-              style={{ color: "#f58634" }}
-            ></i>
-            Contact Us
-          </a>
-          <a href="#tracker">
-            <i
-              className="fa-solid fa-location-dot trc-ker"
-              style={{ color: "#f58634" }}
-            ></i>
-            Tracker
-          </a>
-          <a href="#cart">
-            <i
-              className="fa-solid fa-cart-shopping"
-              style={{ color: "#f58634" }}
-            ></i>
-            (0)
-          </a>
-          <i
-            className="fa-solid fa-right-from-bracket lo-ut"
-            style={{ color: "#f58634" }}
-          ></i>
-        </landing-nav> */}
-
-        {/* <!-- Mobile Menu Toggle -->
-      <!-- Mobile Menu Toggle --> */}
-        {/* <div className="menu-toggle">
-          <svg
-            id="menu-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            className="to-gl"
-            viewBox="0 0 24 24"
-            width="24px"
-            height="24px"
-          >
-            <path fill="#000" d="M0 3h24v2H0zM0 11h24v2H0zM0 19h24v2H0z" />
-          </svg>
-          <img
-            id="close-icon"
-            src="./img/close.png"
-            style={{ display: "none" }}
-            width="24px"
-            height="24px"
-          />
-        </div> */}
-
         {/* <!-- Mobile Menu --> */}
         <div
           className="mobile-menu"
@@ -208,7 +127,7 @@ const LandingPage = () => {
           <i className="fas fa-arrow-down"></i>
         </a>
       </div>
-    </>
+    </div>
   );
 };
 
--- frontend/src/components/ProductPage/ProductPage.jsx
+++ frontend/src/components/ProductPage/ProductPage.jsx
@@ -5,13 +5,9 @@ import MainCarousel from "./Carousel/ThreeProduct";
 const Home = () => {
   return (
     <div className="background py-12 bg-cover bg-no-repeat bg-center min-h-lvh space-y-10 ">
-      <h2 className="md:text-2xl text-black text-center font-semibold">
-        3 Slides 3 Product Each
-      </h2>
+      {/* <h2 className="md:text-2xl text-black text-center font-semibold"></h2> */}
       <MainCarousel />
-      <h2 className="md:text-2xl text-black text-center font-semibold">
-        5 Slides 3 Product Each
-      </h2>
+      {/* <h2 className="md:text-2xl text-black text-center font-semibold"></h2> */}
       <ProductPage />
     </div>
   );
